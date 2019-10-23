7d108604d37d91a7096f1f4b4b2148b0a69a2167
Jorge_JJG <48150256+Jo19r85ge@users.noreply.github.com>
2019-10-23 21:26:52 +0200

    Update Repair.js

--- Scripts/Models/Repair.js
+++ Scripts/Models/Repair.js
@@ -5,10 +5,8 @@ class Repair
         this.ClientName = client === undefined ? "" : client.Name;
         this.Car = client === undefined ? "" : client.Car;
         this.Worker = worker === undefined ? "" : worker.Name;
-
         this.LendedOn = new Date();
         this.ExpiresOn = new Date();
-
         this.LendedOn = (this.LendedOn.getDate() + "/" + (this.LendedOn.getMonth() +1) + "/" + this.LendedOn.getFullYear());
         this.ExpiresOn = (this.ExpiresOn.getDate() + "/" + (this.ExpiresOn.getMonth() +2) + "/" + this.ExpiresOn.getFullYear());
     }
